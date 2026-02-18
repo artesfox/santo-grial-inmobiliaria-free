f8c3b7857b623f499dff7a3cbc65e2618b77b376
artesfox <102455249+artesfox@users.noreply.github.com>
2026-02-17 20:05:54 -0500

    Update index2.js

--- functions/index2.js
+++ functions/index2.js
@@ -129,7 +129,10 @@ function generarPlantilla(tarjetas, total, c) {
         :root { --color-maestro: ${c.color}; }
         body{ background: #f8f9fa; }
         .banner-inicio .contenedor { 
-            background-image: linear-gradient(120deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.3) 100%), url(${c.bannerImg}); 
+            background-image: linear-gradient(120deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.3) 100%), url(${c.bannerImg});
+            }
+         .relleno-5 { padding: 10px 20px 55px 20px; }
+        .houzez-icon.icon-Filter-Faders{ font-size: 30px; 
         }
     </style>
 </head>
@@ -337,6 +340,7 @@ function generarPlantilla(tarjetas, total, c) {
 </body>
 </html>`;
 }
+
 
 
 
