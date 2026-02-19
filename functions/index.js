4609b68f77b01ad896e24d0ea0ab3fb6e3a6dc57
artesfox <102455249+artesfox@users.noreply.github.com>
2026-02-19 01:09:05 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -138,11 +138,14 @@ function generarPlantilla(tarjetas, total, c) {
     <link rel="stylesheet" type="text/css" href="css/icons.css">
     <script src="script.js"></script>
     <style>
-        :root { --color-maestro: ${c.color}; }
+        :root { --color-maestro: ${c.color};
+		       --rgb-maestro: ${rgbMaestro};}
         body{ background: #f8f9fa; }
         .banner-inicio .contenedor { 
-            background-image: linear-gradient(120deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.3) 100%), url(imagenes/bg-2.jpg);
-            }
+       .banner-inicio .contenedor { 
+        background: linear-gradient(180deg, rgba(${rgbMaestro}, 0.67) 0%, rgba(0, 0, 0, 0.84) 100%), 
+                    url(https://santo-grial-inmobiliaria-free.pages.dev/imagenes/bg-2.jpg) !important;
+    }
          .relleno-5 { padding: 10px 20px 55px 20px; }
         .houzez-icon.icon-Filter-Faders{ font-size: 30px; 
         }
@@ -392,6 +395,7 @@ function generarPlantilla(tarjetas, total, c) {
 </body>
 </html>`;
 }
+
 
 
 
